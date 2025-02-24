9993283928cd75a92f82967f24a010413e9f9e83
kursatsmsek <kursatsimsek@protonmail.ch>
2025-02-24 16:02:59 +0300

    validation service and endpoint was completed

--- src/main/java/org/devkursat/javaschematronvalidation/controller/ValidationController.java
+++ src/main/java/org/devkursat/javaschematronvalidation/controller/ValidationController.java
@@ -1,14 +1,29 @@
 package org.devkursat.javaschematronvalidation.controller;
 
+import org.devkursat.javaschematronvalidation.service.ValidationService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ValidationController {
 
+    private final ValidationService validationService;
+
+    public ValidationController(ValidationService validationService) {
+        this.validationService = validationService;
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> hello() {
+        return ResponseEntity.ok().build();
+    }
+
     @PostMapping("/validate")
-    public ResponseEntity validate() {
+    public ResponseEntity<?> validate() throws Exception {
+        validationService.validateXmlWithXslt("src/main/resources/input.xml",
+                "src/main/resources/schematron.xslt");
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/org/devkursat/javaschematronvalidation/service/ValidationService.java
+++ src/main/java/org/devkursat/javaschematronvalidation/service/ValidationService.java
@@ -1,2 +1,46 @@
-package org.devkursat.javaschematronvalidation.service;public class ValidationService {
+package org.devkursat.javaschematronvalidation.service;
+
+import net.sf.saxon.s9api.*;
+import org.springframework.stereotype.Service;
+
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class ValidationService {
+
+    private String readXmlFile(String filePath) throws Exception {
+        return Files.readString(Path.of(filePath), StandardCharsets.UTF_8);
+    }
+
+    public void validateXmlWithXslt(String xmlFilePath, String xsltFilePath) throws Exception {
+        Processor processor = new Processor(false);
+
+        String xmlText = readXmlFile(xmlFilePath);
+        XdmNode document = processor.newDocumentBuilder().build(new XmlSource(xmlText));
+
+        XsltCompiler compiler = processor.newXsltCompiler();
+
+        XsltExecutable executable = compiler.compile(new File(xsltFilePath));
+
+        Xslt30Transformer transformer = executable.load30();
+
+        StringWriter resultWriter = new StringWriter();
+        Serializer serializer = processor.newSerializer(resultWriter);
+
+        transformer.transform(document.asSource(), serializer);
+
+        String output = resultWriter.toString();
+        System.out.println(output);
+    }
+
+    private static class XmlSource extends StreamSource {
+        public XmlSource(String xmlContent) {
+            super(new java.io.StringReader(xmlContent));
+        }
+    }
 }
